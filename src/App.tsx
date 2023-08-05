ec61ed686da62263e293d62fe5403fbe177d2986
home <catalinpce@gmail.com>
2023-08-05 12:26:29 +0300

    firebase setup and folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import Layout from './layout'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function App() {
-  return <label>Homepage</label>
+  return (
+    <Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<label>Homepage</label>} />
+        </Routes>
+      </BrowserRouter>
+    </Layout>
+  )
 }
