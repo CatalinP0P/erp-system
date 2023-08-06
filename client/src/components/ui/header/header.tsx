a221e6d350abeac406be56f992a977b8316c00b6
home <catalinpce@gmail.com>
2023-08-06 12:11:33 +0300

    added auth system

--- client/src/components/ui/header/header.tsx
+++ client/src/components/ui/header/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../others/logo/logo'
 import SettingsIcon from '@mui/icons-material/Settings'
 import profileBlank from '../../../assets/profileblank.webp'
@@ -6,7 +6,10 @@ import profileBlank from '../../../assets/profileblank.webp'
 import { Notifications } from '@mui/icons-material'
 import { MenuRounded } from '@mui/icons-material'
 import { ClearRounded } from '@mui/icons-material'
+import { useAuth } from '../../../context/authContext'
+
 import './header.scss'
+import { useScrollTrigger } from '@mui/material'
 
 export default function Header({
   setMobileMenu,
@@ -15,6 +18,10 @@ export default function Header({
   setMobileMenu?: any
   mobileMenu?: boolean
 }) {
+  const { user, loading, signOut } = useAuth()
+
+  const [accountDropdown, setAccountDropdown] = useState<boolean>(false)
+
   return (
     <div className="header">
       <div className="header__container">
@@ -40,14 +47,33 @@ export default function Header({
           <div className="header__button">
             <Notifications />
           </div>
-          <div className="profile--card">
+          <div
+            className="profile--card"
+            onClick={() => setAccountDropdown(!accountDropdown)}
+          >
             <div className="profile--card__container">
-              <label className="profile--card__name">Catalin Pop</label>
-              <label className="profile--card__email">
-                catalinpce@gmail.com
+              <label className="profile--card__name">{user?.displayName}</label>
+              <label className="profile--card__email">{user?.email}</label>
+            </div>
+            <img
+              src={user?.photoURL ? user.photoURL : profileBlank}
+              className="profile--card__image"
+            />
+            <div
+              className={
+                'profile--card__dropdown ' + (accountDropdown ? ' active' : '')
+              }
+            >
+              <label className="profile--card__dropdown__item">
+                Hi, <strong>{user?.email}</strong>
+              </label>
+              <label
+                onClick={() => signOut()}
+                className="profile--card__dropdown__item"
+              >
+                Sign out
               </label>
             </div>
-            <img src={profileBlank} className="profile__card__image" />
           </div>
         </div>
       </div>
